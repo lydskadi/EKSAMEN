9013a52f7c67328237da91b34251b4ee6a4f2873
lydskadi <arab.inc@mail.ru>
2022-06-15 18:55:00 +0700

    63

--- Tag.php
+++ Tag.php
@@ -26,7 +26,11 @@
 			$result = '';
 			
 			foreach ($attrs as $name => $value) {
-				$result .= " $name=\"$value\"";
+				if ($value === true) {
+					$result .= " $name"; 
+				} else {
+					$result .= " $name=\"$value\""; 
+				}
 			}
 			
 			return $result;
--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once 'tag.php';
 	
 	$tag = new Tag('input');
 	echo $tag
-		->setAttrs(['id' => 'test', 'class' => 'eee']) // задаем атрибуты массивом
-		->open(); // выведет <input id="test" class="eee">
+		->setAttr('id', 'test')
+		->setAttr('disabled', true)
+		->open(); // выведет <input id="test" disabled>
 ?>
