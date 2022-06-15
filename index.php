b9ff8f825b4f42a17148102c2eb69bd52e8750f8
lydskadi <arab.inc@mail.ru>
2022-06-15 18:56:11 +0700

    64

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php 
 require_once 'tag.php';
 	
-	$tag = new Tag('input');
-	echo $tag
-		->setAttr('id', 'test')
-		->setAttr('disabled', true)
-		->open(); // выведет <input id="test" disabled>
+	echo (new Tag('input'))->setAttr('name', 'name1')->open();
+	echo (new Tag('input'))->setAttr('name', 'name2')->open();
 ?>
+
+
