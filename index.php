3213823b4814d47876226122ac65088319103e51
lydskadi <arab.inc@mail.ru>
2022-06-15 18:39:00 +0700

    57

--- index.php
+++ index.php
@@ -1,25 +1,16 @@
 
 <?php 
 require_once 'date.php';
-	$date = new Date('2025-12-31');
+require_once 'interval.php';
 	
-	echo $date->getYear(); echo ' ';  // выведет '2025'
-	echo $date->getMonth(); echo ' '; // выведет '12'
-	echo $date->getDay(); echo ' '; // выведет '31'	
+	$date1 = new Date('2025-12-31');
+	$date2 = new Date('2026-11-28');
 	
-	echo '<br>';
+	$interval = new Interval($date1, $date2);
 	
-	echo $date->getWeekDay(); echo ' ';    // выведет '3'
-	echo $date->getWeekDay('ru'); echo ' '; // выведет 'среда'
-	echo $date->getWeekDay('en'); echo ' '; // выведет 'wednesday'
-	
+	echo $interval->toDays();   // выведет разницу в днях
 	echo '<br>';
-	
-	echo (new Date('2025-12-31'))->addYear(1); 
+	echo $interval->toMonths(); // выведет разницу в месяцах
 	echo '<br>';
-	echo (new Date('2025-12-31'))->addDay(1);  
-	echo '<br>';
-	echo (new Date('2025-12-31'))->subDay(3); 
-	echo '<br>';
-	echo (new Date('2025-12-28'))->addYear(1); 
+	echo $interval->toYears();  // выведет разницу в годах
 ?>
