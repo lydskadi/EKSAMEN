d6a2cc1094478559eae8e34c1fedd73fa31926e7
lydskadi <arab.inc@mail.ru>
2022-06-15 19:29:59 +0700

    77

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 	require_once 'Form.php';
 	require_once 'Input.php';
 	require_once 'Submit.php';
-	require_once 'pas.php';
 	require_once 'Hidden.php';
-	require_once 'Textarea.php';
+	require_once 'Checkbox.php';
 	
-	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
+	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 	
 	echo $form->open();
+		echo (new Checkbox)->setAttr('name', 'checkbox');
 		echo (new Input)->setAttr('name', 'user');
-		echo (new Textarea)->setAttr('name', 'message')->show();
 		echo new Submit;
 	echo $form->close();
 ?>
