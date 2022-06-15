b1adbb7e322306747901274c6941ec7ce5068312
lydskadi <arab.inc@mail.ru>
2022-06-15 18:53:16 +0700

    62

--- Tag.php
+++ Tag.php
@@ -46,6 +46,13 @@
 			unset($this->attrs[$name]);
 			}
 		return $this;
-		}		
-	} 
+		}
+	public function setAttrs($attrs)
+		{
+			foreach ($attrs as $name => $value) {
+			$this->setAttr($name, $value);
+		}
+		return $this;
+	}
+} 
 ?>
--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 require_once 'tag.php';
 	
 	$tag = new Tag('input');
-	echo $tag->setAttr('id', 'test')->setAttr('class', 'eee bbb')->open();
+	echo $tag
+		->setAttrs(['id' => 'test', 'class' => 'eee']) // задаем атрибуты массивом
+		->open(); // выведет <input id="test" class="eee">
 ?>
