bf2a86eda8f660f3b636a8a4b50fb1f6992afdfa
lydskadi <arab.inc@mail.ru>
2022-06-15 19:25:06 +0700

    73

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
 	require_once 'Form.php';
 	require_once 'Input.php';
+	require_once 'Submit.php';
 	
-	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
+	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
+	
 	echo $form->open();
-		echo (new Input())->setAttrs(['name'=>'num1', 'value' => '1']);
-		echo (new Input())->setAttrs(['name'=>'num2', 'value' => '2']);
-		echo (new Input())->setAttrs(['name'=>'num3', 'value' => '3']);
-		echo (new Input())->setAttrs(['name'=>'num4', 'value' => '4']);
-		echo (new Input())->setAttrs(['name'=>'num5', 'value' => '5']);
-		echo (new Input())->setAttrs(['type'=>'submit']);
+		echo (new Input)->setAttr('name', 'year');
+		echo new Submit;
 	echo $form->close();
-
-	if(isset($_GET['num1'])){
-	echo $sum = $_GET['num1'] + $_GET['num2'] + $_GET['num3'] + $_GET['num4'] + $_GET['num5'];
-	}
 ?>
