44172e579f7e640cb1c0b74214b7a8d98c0c0996
lydskadi <arab.inc@mail.ru>
2022-06-15 19:11:26 +0700

    69 1-5

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	require_once 'image.php';
-	$img = (new Image())->setAttrs(['src' => 'img.png', 'width' =>'300', 'height'=>'200']);
-	echo $img->open();
+	require_once 'Link.php';
+	require_once 'menu.php';
+	echo $link1. ' '. $link2.' ' . $link3. ' '. $link4. ' '. $link5."<br>";
 ?>
 
