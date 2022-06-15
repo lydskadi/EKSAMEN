a6b7837e1aaf935d90c4d7ab505261b4d3a0da76
lydskadi <arab.inc@mail.ru>
2022-06-15 19:17:12 +0700

    70.1

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-	require_once 'Link.php';
-	echo (new Link)->setAttr('href', '/php/56-78/index.php')->setText('index')->show();
+	require_once 'HtmlList.php';
+	$list = new HtmlList('ul');
+	
+	echo $list
+		->addItem((new ListItem())->setText('item1'))
+		->addItem((new ListItem())->setText('item2'))
+		->addItem((new ListItem())->setText('item3'))
+		->show();
 ?>
