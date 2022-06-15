2e427a5e5535f22c4cb2582f8a0c5cf5dcc742d9
lydskadi <arab.inc@mail.ru>
2022-06-15 18:46:39 +0700

    60.1 q git commit -a -m 60.1

--- Tag.php
+++ Tag.php
@@ -2,22 +2,36 @@
 	class Tag
 	{
 		private $name;
-
+		
 		public function __construct($name)
 		{
 			$this->name = $name;
 		}
-
+		
 		public function open()
 		{
 			$name = $this->name;
 			return "<$name>";
 		}
-
+		
 		public function close()
 		{
 			$name = $this->name;
 			return "</$name>";
 		}
+		private function getAttrsStr($attrs)
+	{
+		if (!empty($attrs)) {
+			$result = '';
+			
+			foreach ($attrs as $name => $value) {
+				$result .= " $name=\"$value\"";
+			}
+			
+			return $result;
+		} else {
+			return '';
+		}
+	}
 	}
 ?>
