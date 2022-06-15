005231dae9e6842209f651693eaf0bf8eb1b8daf
lydskadi <arab.inc@mail.ru>
2022-06-15 18:44:19 +0700

    59 1-3

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-
 <?php 
-require_once 'iFile.php';
-require_once 'File.php';
+require_once 'tag.php';
 	
-	$file = new File('index.php');
-
-echo $file->getPath().'<br>';
-echo $file->getDir().'<br>';
-echo $file->getExt().'<br>';
-echo $file->getName().'<br>';
-echo $file->getSize().' байт'.'<br>';
-
-echo $file->getText();
+	$img = new Tag('img');
+	echo $img->open().''.$img->close(); 
 	
+	$header = new Tag('header');
+	echo $header->open().'header сайта'.$header->close();
 ?>
