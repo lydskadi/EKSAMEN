29e11b52a098586501820cd8f241edc84d45705c
lydskadi <arab.inc@mail.ru>
2022-06-15 19:26:17 +0700

    74

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 	require_once 'Form.php';
 	require_once 'Input.php';
 	require_once 'Submit.php';
+	require_once 'pas.php';
 	
 	$form = (new Form)->setAttrs(['action' => 'test.php', 'method' => 'GET']);
 	
 	echo $form->open();
-		echo (new Input)->setAttr('name', 'year');
+		echo (new Input)   ->setAttr('name', 'login');
+		echo (new pas)->setAttr('name', 'password');
 		echo new Submit;
 	echo $form->close();
 ?>
